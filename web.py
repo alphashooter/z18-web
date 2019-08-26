4295ef850a3650aad1328c5b869200018b2b8bb2
Andrey Strelkov <a.p.strelkov@gmail.com>
2019-08-26 22:51:27 +0300

    registration

--- web.py
+++ web.py
@@ -1,6 +1,12 @@
-from flask import Flask, Response, make_response, render_template, request
+from flask import Flask, render_template, request, redirect
+from sqlite3 import connect
+
 
 app = Flask(__name__, static_url_path='/static')
+
+
+def get_db():
+    return connect('db.sqlite3')
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -10,6 +16,20 @@ def index():
         params["name"] = request.form["name"]
     return render_template('index.html', **params)
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    email = request.form['email']
+    password = request.form['password']
+    db = get_db()
+    db.execute(
+        'insert into users (email, password) values (?, ?)',
+        [email, password]
+    )
+    db.commit()
+    return redirect('/')
 
 
 app.run()
