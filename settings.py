163e85c7defc8ecd28a0e909b2526d27a3005e6c
Andrey Strelkov <a.p.strelkov@gmail.com>
2019-09-07 16:24:40 +0300

    migration to django

--- settings.py
+++
@@ -1,6 +0,0 @@
-from sqlite3 import connect, Row
-
-
-secret = '7zS6PLhQdr'
-db = connect('db.sqlite3', check_same_thread=False)
-db.row_factory = Row
---
+++ webapp/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.views import View
+from django.http.request import *
+from django.http.response import *
+from django.contrib.auth import authenticate, login, logout
+from .models import *
+
+# Create your views here.
+
+
+def index(request: HttpRequest):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+
+def product(request: HttpRequest, product_id: int):
+    try:
+        product = Product.objects.get(pk=product_id)
+    except Product.DoesNotExist:
+        return HttpResponseNotFound()
+    return render(request, 'product.html', {'product': product})
+
+
+class LoginView(View):
+    def get(self, request: HttpRequest):
+        return render(request, 'login.html')
+
+    def post(self, request: HttpRequest):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return render(request, 'login.html')
+        login(request, user)
+        return redirect('/')
+
+
+class RegisterView(View):
+    def get(self, request: HttpRequest):
+        return render(request, 'register.html')
+
+    def post(self, request: HttpRequest):
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except:
+            return render(request, 'register.html')
+        login(request, user)
+        return redirect('/')
